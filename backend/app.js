4e32cc6e0a8f4e0c75187cc1bf1d71302f1bf539
pouryawJS <pourya.bu@gmail.com>
2025-07-25 15:03:45 +0330

    Feat: Error Handler middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
+const { errorHandler } = require("./middlewares/errorHandler");
 
 const authRoutes = require("./modules/Auth/auth.routes");
 const userRoutes = require("./modules/User/user.routes");
@@ -9,14 +9,24 @@ const apiDocRouter = require("./modules/ApiDoc/swagger.routes");
 
 const app = express();
 
+//* Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+//* CORS Policy
 app.use(cors());
 
+//* Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
 app.use("/api-doc", apiDocRouter);
+
+//* Error Handler
+app.use((req, res) => {
+	return res.status(404).json({ msg: "Not Found Path" });
+});
+
+app.use(errorHandler);
 
 module.exports = app;
