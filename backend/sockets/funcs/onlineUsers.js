2dd12b3370bee446991577488caf590b3a0ce55f
pouryawJS <pourya.bu@gmail.com>
2025-08-29 16:33:21 +0330

    Fix: Rename

--- backend/sockets/funcs/onlineUsers.js
+++ backend/sockets/funcs/onlineUsers.js
@@ -1,4 +1,4 @@
 exports.roomOnlineUsersCount = async (io, roomId) => {
 	const count = io.sockets.adapter.rooms.get(roomId)?.size || 0;
-	io.to(roomId).emit("chat:get:onlineUsers", count);
+	io.to(roomId).emit("chat:onlineUsers", count);
 };
